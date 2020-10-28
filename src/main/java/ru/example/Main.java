858c7fe6e382804cb8d9a818d69021cf99e39bce
Andrey Patsalyuk <pazalyuk@mail.com>
2020-10-28 22:11:31 +0300

    Add method sumTwoNumbers and its testing

--- src/main/java/ru/example/Main.java
+++ src/main/java/ru/example/Main.java
@@ -2,6 +2,12 @@ package ru.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi!");
+        byte a = 5;
+        short b = 10;
+        System.out.println(sumTwoNumbers(a, b));
+    }
+
+    public static byte sumTwoNumbers(byte value1, short value2) {
+        return (byte) (value1 + value2);
     }
 }
