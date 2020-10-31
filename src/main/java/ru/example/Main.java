e69088cb9b78b7ba2b35479621f0ee89f32df304
Andrey Patsalyuk <pazalyuk@gmail.com>
2020-10-31 18:08:27 +0300

    Add methon getMaxNumber

--- src/main/java/ru/example/Main.java
+++ src/main/java/ru/example/Main.java
@@ -3,9 +3,10 @@ package ru.example;
 public class Main {
     public static void main(String[] args) {
         byte a = 5;
-        short b = 10;
-        System.out.println(sumTwoNumbers(a, b));
-        System.out.println(multiplyTwoNumbers(a, b));
+        byte b = 10;
+        System.out.println("Sum of two numbers: " + sumTwoNumbers(a, b));
+        System.out.println("Product of two numbers:" + multiplyTwoNumbers(a, b));
+        System.out.println("Max of two numbers: " + getMaxNumber(10, 10));
     }
 
     public static byte sumTwoNumbers(byte a, short b) {
@@ -14,6 +15,10 @@ public class Main {
 
     public static int multiplyTwoNumbers(int a, long b) {
         return (int) (a * b);
+    }
+
+    public static int getMaxNumber(int a, int b){
+        return (a > b) ? a : b;
     }
 
 }
