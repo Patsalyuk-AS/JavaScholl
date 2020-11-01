53645995d015f2f368057882405aac6247d8261c
Andrey Patsalyuk <pazalyuk@gmail.com>
2020-11-01 13:59:52 +0300

    Add: isCharA isCharNumber printBinary printIntegerMaxBinary getIntFromChar getCharFromInt

--- src/main/java/ru/example/Main.java
+++ src/main/java/ru/example/Main.java
@@ -6,19 +6,55 @@ public class Main {
         byte b = 10;
         System.out.println("Sum of two numbers: " + sumTwoNumbers(a, b));
         System.out.println("Product of two numbers:" + multiplyTwoNumbers(a, b));
-        System.out.println("Max of two numbers: " + getMaxNumber(10, 10));
+        System.out.println("Max of two numbers: " + getMaxNumber(a, b));
+        System.out.println(isCharA('A'));
+        System.out.println(isCharNumber('0'));
+        printBinary();
+        printIntegerMaxBinary();
+        System.out.println(getIntFromChar('A'));
+        System.out.println(getCharFromInt(65));
     }
 
     public static byte sumTwoNumbers(byte a, short b) {
+
         return (byte) (a + b);
     }
 
     public static int multiplyTwoNumbers(int a, long b) {
+
         return (int) (a * b);
     }
 
     public static int getMaxNumber(int a, int b){
+
         return (a > b) ? a : b;
+    }
+
+    public static boolean isCharA(char ch){
+        return (ch == 'A')? true: false;
+    }
+
+    public static boolean isCharNumber(char ch){
+        return Character.isDigit(ch);
+    }
+
+    public static void printBinary(){
+        for(int i = 0; i <=30; i++){
+            System.out.println(i + ": " + Integer.toBinaryString(i));
+        }
+    }
+
+    public static void  printIntegerMaxBinary(){
+        System.out.println("Integer.MAX_VALUE: " + Integer.toBinaryString(Integer.MAX_VALUE));
+        System.out.println("Integer.MAX_VALUE + 1: " + Integer.toBinaryString(Integer.MAX_VALUE + 1));
+    }
+
+    public static int getIntFromChar(char ch){
+        return ch;
+    }
+
+    public static char getCharFromInt(int a){
+        return (char) a;
     }
 
 }
